11dd532035dc003c00823ff083332fbb7b88af39
Manikandan Venkatraman <vmanikandan001@hotmail.com>
2017-01-08 23:05:40 +0000

    Update wave view

--- app/src/main/java/uk/co/qubitssolutions/bharatradios/app/views/StepVisualizerView.java
+++ app/src/main/java/uk/co/qubitssolutions/bharatradios/app/views/StepVisualizerView.java
@@ -11,25 +11,25 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
-@BindingMethods(@BindingMethod(type = WaveVisualizerView.class, attribute = "app:updateVisualizer", method = "updateVisualizer"))
-public class WaveVisualizerView extends View {
+@BindingMethods(@BindingMethod(type = StepVisualizerView.class, attribute = "app:updateVisualizer", method = "updateVisualizer"))
+public class StepVisualizerView extends View {
 
     private byte[] mBytes;
     private float[] mPoints;
     private Rect mRect = new Rect();
     private Paint mForePaint = new Paint();
 
-    public WaveVisualizerView(Context context) {
+    public StepVisualizerView(Context context) {
         super(context);
         init();
     }
 
-    public WaveVisualizerView(Context context, AttributeSet attrs) {
+    public StepVisualizerView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public WaveVisualizerView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public StepVisualizerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
@@ -57,15 +57,24 @@ public class WaveVisualizerView extends View {
         }
         mRect.set(0, 0, getWidth(), getHeight());
 
+        int mDivisions = 2;
+        boolean mTop = false;
+        mForePaint.setStrokeWidth(12f);
+        for (int i = 0; i < mBytes.length / mDivisions; i++) {
+            mPoints[i * 4] = i * 4 * mDivisions;
+            mPoints[i * 4 + 2] = i * 4 * mDivisions;
+            byte rfk = mBytes[mDivisions * i];
+            byte ifk = mBytes[mDivisions * i + 1];
+            float magnitude = (rfk * rfk + ifk * ifk);
+            int dbValue = (int) (10 * Math.log10(magnitude));
 
-        for (int i = 0; i < mBytes.length - 1; i++) {
-            mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
-            mPoints[i * 4 + 1] = mRect.height() / 2
-                    + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
-            mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
-            mPoints[i * 4 + 3] = mRect.height() / 2
-                    + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2)
-                    / 128;
+            if (mTop) {
+                mPoints[i * 4 + 1] = 0;
+                mPoints[i * 4 + 3] = (dbValue * 2 - 10);
+            } else {
+                mPoints[i * 4 + 1] = mRect.height();
+                mPoints[i * 4 + 3] = mRect.height() - (dbValue * 2 - 10);
+            }
         }
 
         canvas.drawLines(mPoints, mForePaint);
